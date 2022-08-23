9bb3c78c5c6649f6c640b270b8f2e438a6b0c202
iamggpanda <girish.ganesan90@gmail.com>
2022-08-23 10:33:46 +0900

    First gmmk commit

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/iamggpanda/config.h
@@ -0,0 +1,5 @@
+// path: qmk_firmware/keyboards/gmmk/pro/ansi/keymaps/iamggpanda/config.h
+#pragma once
+#ifdef RGB_MATRIX_ENABLE
+    #define RGB_DISABLE_WHEN_USB_SUSPENDED true
+#endif // RGB_MATRIX_ENABLE
---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/iamggpanda/rules.mk
@@ -0,0 +1,3 @@
+# path: qmk_firmware/keyboards/gmmk/pro/ansi/keymaps/iamggpanda/rules.mk
+VIA_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
